d2efbe94b457c59766ad35eb4a99b39a3001ce7a
eslameltaher <eslam.eltaher@hotmail.com>
2019-08-19 03:48:26 +0200

    understanding redux basics

---
+++ redux-basics.js
@@ -0,0 +1,33 @@
+const createStore = require('redux').createStore;
+
+const initialState = {
+    counter : 0
+};
+
+//Reducer
+const rootReducer = (state = initialState, action) => {
+    if(action != null) {
+        switch(action.type)
+        {
+            case "INC_COUNTER":
+                return { ...state, counter: state.counter + 1 };
+            case "ADD_COUNTER":
+                return { ...state, counter: state.counter +  action.value};
+        }
+    }
+    return state;
+};
+
+//Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+//Subscription
+store.subscribe(() => {
+    console.log("[Subscription] ", store.getState());
+});
+
+//Dispatching Action
+store.dispatch({ type : "INC_COUNTER" });
+store.dispatch({ type : "ADD_COUNTER", value: 10 });
+console.log(store.getState());
