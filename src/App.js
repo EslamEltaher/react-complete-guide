80629f6e3765f4807a1e9178a60001c520b67a6b
eslam.eltaher <eslam.mohamed@linkdev.com>
2019-08-22 16:51:15 +0200

    added redux to project

--- src/App.js
+++ src/App.js
@@ -2,42 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Persons from './components/Persons';
 import AddPerson from './components/AddPerson/AddPerson';
+import { connect } from 'react-redux';
+import * as actionTypes from './store/actionTypes'
 
 class App extends Component {
+  componentDidMount = () => {
+    this.props.storeDispatch({ type: actionTypes.ADD_PERSON, personData: { name: "Eslam",  age: 26 } });
+    this.props.storeDispatch({ type: actionTypes.ADD_PERSON, personData: { name: "Maximilian",  age: 28 } });
+    this.props.storeDispatch({ type: actionTypes.ADD_PERSON, personData: { name: "Manu",  age: 29 } });
+  }
+
   state = { 
-    people : [
-      { id: 1, name : "Eslam", age : 26 },
-      { id: 2, name : "Maximilian", age : 28 },
-      { id: 3, name : "Manu", age : 29 },
-    ],
     otherState: "Ay 7aga"
   };
-
-  // switchNamesHandler = () => {
-  //   this.setState({ 
-  //     people : [
-  //       { id: 1, name : "Eslaaaam", age : 26 },
-  //       { id: 2, name : "Maximilian", age : 28 },
-  //       { id: 3, name : "Manu", age : 27 },
-  //     ]
-  //   });
-  // };
-
-  deletePersonHandler = (id) => {
-    this.setState(prevState => {
-      let people = prevState.people;
-
-      return {
-        people: people.filter(p => p.id !== id)
-      }
-    })
-  }
-
-  addPersonHandler = (name, age) => {
-    this.setState(prevstate => ({
-      people: prevstate.people.concat({ id: Math.floor(Math.random() * 3000), name: name, age: age })
-    }));
-  }
 
   style = {
     backgroundColor: 'white',
@@ -59,15 +36,27 @@ class App extends Component {
           </nav>
         </header>
         <h1>My First React App</h1>
-        <Persons people={this.state.people} click={this.deletePersonHandler}/>
+        <Persons people={this.props.people} click={this.props.removePerson}/>
         <button style={this.style} onClick={this.switchNamesHandler} type="button">Swtich names</button>
         <br/>
         <br/>
-        <AddPerson addPerson={this.addPersonHandler}/>
+        <AddPerson addPerson={this.props.addPerson}/>
       </div>
     );
   }
   // return React.createElement('div', { className : 'App' }, React.createElement('h1', null, 'My First React App'));
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return { 
+    people : state.people
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  storeDispatch : dispatch,
+  addPerson: (name, age) => dispatch({ type: actionTypes.ADD_PERSON, personData: { name, age } }),
+  removePerson: (id) => dispatch({ type: actionTypes.REMOVE_PERSON, personId: id})
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+import * as actionTypes from './actionTypes';
+
+const initialState = {
+    people : []
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actionTypes.ADD_PERSON:
+            return {
+                ...state,
+                people: state.people.concat({ 
+                    id: Math.floor(Math.random() * 3000), 
+                    name: action.personData.name, 
+                    age: action.personData.age 
+                })
+            }
+        case actionTypes.REMOVE_PERSON:
+            return {
+                ...state,
+                people: state.people.filter(p => p.id !== action.personId)
+            };
+    }
+
+    return state;
+}
+
+export default reducer;
